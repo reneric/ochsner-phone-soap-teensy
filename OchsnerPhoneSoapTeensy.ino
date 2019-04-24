1e9407c5c08f5699ae1e3d1c653bd851aaf0a19c
Ren Simmons <rsimmo4@gmail.com>
2019-04-24 11:49:37 -0500

    add blues

--- OchsnerPhoneSoapTeensy.ino
+++ OchsnerPhoneSoapTeensy.ino
@@ -1,6 +1,6 @@
 #include <FastLED.h>
 
-#define DEBUG 1
+#define DEBUG 0
 
 FASTLED_USING_NAMESPACE
 
@@ -11,7 +11,7 @@ FASTLED_USING_NAMESPACE
 
 CRGB leds[NUM_LEDS];
 
-#define BRIGHTNESS          96
+#define BRIGHTNESS         200
 #define FRAMES_PER_SECOND  120
 
 // The time (ms) for the "complete" state
@@ -31,11 +31,6 @@ long completeStateTimer = 0;
 boolean deviceOff ();
 
 CRGBPalette16 currentPalette;
-TBlendType    currentBlending;
-
-extern CRGBPalette16 myRedWhiteBluePalette;
-extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
-
 
 void setup() {
   delay(3000); // 3 second delay for recovery
@@ -50,9 +45,6 @@ void setup() {
   // Set master brightness control
   FastLED.setBrightness(BRIGHTNESS);
   currentState = 0;
-
-  currentPalette = RainbowColors_p;
-  currentBlending = LINEARBLEND;
 }
 
 void loop()
@@ -110,8 +102,12 @@ void setActive () {
 #if DEBUG == 1  
   Serial.println("ACTIVE");
 #endif
-  fill_solid(leds, NUM_LEDS, CRGB::Blue);
-  // juggle();
+  // fill_solid(leds, NUM_LEDS, CRGB::Blue);
+    
+    static uint8_t startIndex = 0;
+    startIndex = startIndex + 1; /* motion speed */
+    
+    FillLEDsFromPaletteColors( startIndex);
 }
 
 void setComplete () {
@@ -125,7 +121,7 @@ void setIdle () {
 #if DEBUG == 1
   Serial.println("IDLE");
 #endif
-  fill_solid(leds, NUM_LEDS, CRGB::White);
+  fill_solid(leds, NUM_LEDS, CRGB::Gray);
 }
 
 uint8_t gHue = 0;
@@ -183,98 +179,23 @@ void juggle() {
 
 void FillLEDsFromPaletteColors( uint8_t colorIndex)
 {
-    uint8_t brightness = 255;
-    
     for( int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
-        colorIndex += 3;
+        leds[i] = ColorFromPalette( SetupActivePalette(), colorIndex, BRIGHTNESS, LINEARBLEND);
+        colorIndex += 5;
     }
 }
 
-void ChangePalettePeriodically()
+CRGBPalette16 SetupActivePalette()
 {
-    uint8_t secondHand = (millis() / 1000) % 60;
-    static uint8_t lastSecond = 99;
+    CRGB blue = CHSV( HUE_BLUE, 255, 255);
+    CRGB darkBlue  = CRGB::DarkBlue;
+    CRGB black  = CRGB::DarkBlue;
     
-    if( lastSecond != secondHand) {
-        lastSecond = secondHand;
-        if( secondHand ==  0)  { currentPalette = RainbowColors_p;         currentBlending = LINEARBLEND; }
-        if( secondHand == 10)  { currentPalette = RainbowStripeColors_p;   currentBlending = NOBLEND;  }
-        if( secondHand == 15)  { currentPalette = RainbowStripeColors_p;   currentBlending = LINEARBLEND; }
-        if( secondHand == 20)  { SetupPurpleAndGreenPalette();             currentBlending = LINEARBLEND; }
-        if( secondHand == 25)  { SetupTotallyRandomPalette();              currentBlending = LINEARBLEND; }
-        if( secondHand == 30)  { SetupBlackAndWhiteStripedPalette();       currentBlending = NOBLEND; }
-        if( secondHand == 35)  { SetupBlackAndWhiteStripedPalette();       currentBlending = LINEARBLEND; }
-        if( secondHand == 40)  { currentPalette = CloudColors_p;           currentBlending = LINEARBLEND; }
-        if( secondHand == 45)  { currentPalette = PartyColors_p;           currentBlending = LINEARBLEND; }
-        if( secondHand == 50)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = NOBLEND;  }
-        if( secondHand == 55)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = LINEARBLEND; }
-    }
+    return CRGBPalette16(
+                                   blue,  blue,  black,  black,
+                                   darkBlue, darkBlue, black,  black,
+                                   blue,  blue,  black,  black,
+                                   darkBlue, darkBlue, black,  black );
 }
-
-// This function fills the palette with totally random colors.
-void SetupTotallyRandomPalette()
-{
-    for( int i = 0; i < 16; i++) {
-        currentPalette[i] = CHSV( random8(), 255, random8());
-    }
-}
-
-// This function sets up a palette of black and white stripes,
-// using code.  Since the palette is effectively an array of
-// sixteen CRGB colors, the various fill_* functions can be used
-// to set them up.
-void SetupBlackAndWhiteStripedPalette()
-{
-    // 'black out' all 16 palette entries...
-    fill_solid( currentPalette, 16, CRGB::Black);
-    // and set every fourth one to white.
-    currentPalette[0] = CRGB::White;
-    currentPalette[4] = CRGB::White;
-    currentPalette[8] = CRGB::White;
-    currentPalette[12] = CRGB::White;
-    
-}
-
-// This function sets up a palette of purple and green stripes.
-void SetupPurpleAndGreenPalette()
-{
-    CRGB purple = CHSV( HUE_PURPLE, 255, 255);
-    CRGB green  = CHSV( HUE_GREEN, 255, 255);
-    CRGB black  = CRGB::Black;
-    
-    currentPalette = CRGBPalette16(
-                                   green,  green,  black,  black,
-                                   purple, purple, black,  black,
-                                   green,  green,  black,  black,
-                                   purple, purple, black,  black );
-}
-
-
-// This example shows how to set up a static color palette
-// which is stored in PROGMEM (flash), which is almost always more
-// plentiful than RAM.  A static PROGMEM palette like this
-// takes up 64 bytes of flash.
-const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM =
-{
-    CRGB::Red,
-    CRGB::Gray, // 'white' is too bright compared to red and blue
-    CRGB::Blue,
-    CRGB::Black,
-    
-    CRGB::Red,
-    CRGB::Gray,
-    CRGB::Blue,
-    CRGB::Black,
-    
-    CRGB::Red,
-    CRGB::Red,
-    CRGB::Gray,
-    CRGB::Gray,
-    CRGB::Blue,
-    CRGB::Blue,
-    CRGB::Black,
-    CRGB::Black
-};
 
 
